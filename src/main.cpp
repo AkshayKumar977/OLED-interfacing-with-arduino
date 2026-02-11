688cbfc4d75426b46a7879953d950a3050064253
AkshayKumar977 <akshay.kumar_cs.aiml24@gla.ac.in>
2026-02-11 09:33:58 +0530

    Initialize OLED display and add error handling in setup

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,15 @@
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 #define OLED_ADDR 0x3C
-
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT);
 void setup() {
 // write your initialization code here
+    Serial.begin(9600);
+    if (!display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR)) {
+        Serial.println("OLED not found");
+        while (true);
+    }
+
 }
 
 void loop() {
